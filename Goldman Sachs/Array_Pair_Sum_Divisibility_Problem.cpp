0afe7f764e1ae4a7302f6ba62ceb6e172dc42412
pranjaladhav27 <86354898+pranjaladhav27@users.noreply.github.com>
2022-01-05 23:06:45 +0530

    Update Array_Pair_Sum_Divisibility_Problem.cpp

--- Goldman Sachs/Array_Pair_Sum_Divisibility_Problem.cpp
+++ Goldman Sachs/Array_Pair_Sum_Divisibility_Problem.cpp
@@ -1,1 +1,13 @@
-
+bool canPair(vector<int> nums, int k) {
+        unordered_map<int,int> m;
+        for(auto i : nums)
+        {
+            m[(i%k+k)%k]++;
+        }
+        if(m[0]%2==1) return false;
+        for(int i=1;i<k;i++)
+        {
+            if(m[i]!=m[k-i]) return false;
+        }
+        return true;
+    }
